70e3aa56ce3ae7a71e9b6b654a8eb0c946615798
Suyeol Jeon <devxoul@gmail.com>
2016-05-26 00:10:47 +0900

    Start Xcode project

---
+++ GenericReusable/GenericReusable.h
@@ -0,0 +1,19 @@
+//
+//  GenericReusable.h
+//  GenericReusable
+//
+//  Created by Suyeol Jeon on 5/26/16.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GenericReusable.
+FOUNDATION_EXPORT double GenericReusableVersionNumber;
+
+//! Project version string for GenericReusable.
+FOUNDATION_EXPORT const unsigned char GenericReusableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GenericReusable/PublicHeader.h>
+
+
